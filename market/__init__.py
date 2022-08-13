3ca3f2aa9988905b6b6a14010d6ac2ef148a8bff
Bunty <chandrashekargattu@gmail.com>
2022-08-13 20:23:17 +0530

    made a change to path

--- market/__init__.py
+++ market/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://vyltqkbfwiatos:202dab04b98cbf1832199a50aa1c1c34abfeda28cda45efb92416c0a1a34ea8e@ec2-3-223-242-224.compute-1.amazonaws.com:5432/da8l04fvu1icuj'
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://vyltqkbfwiatos:202dab04b98cbf1832199a50aa1c1c34abfeda28cda45efb92416c0a1a34ea8e@ec2-3-223-242-224.compute-1.amazonaws.com:5432/da8l04fvu1icuj'
 
 app.config['SECRET_KEY']= 'ff648669200685e80e1de9eb'
 db = SQLAlchemy(app)
